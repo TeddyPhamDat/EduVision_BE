3cc2f4a59e8c0ab30e6a79f4c0468a5e4ff52094
QuangStar <minhquang932004@gmail.com>
2025-06-05 21:24:53 +0700

    Adding swagger for production

--- EduVision/Program.cs
+++ EduVision/Program.cs
@@ -101,11 +101,8 @@ builder.Services.AddSingleton<TextToSpeechService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseExceptionHandler(errorApp =>
 {
